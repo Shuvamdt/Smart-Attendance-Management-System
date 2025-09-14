cb50ea0786c321e9f0c7652ee197ed48796dbe1b
Shuvamdt <shuvamdt1230@gmail.com>
2025-09-14 20:31:48 +0530

    update

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Registration from "./Pages/Registration";
 import Home from "./Pages/Home";
-import ClassTabStudent from "./Pages/ClasstabStudent";
+import ClassTabStudent from "./Pages/ClassTabStudent";
 import Admin from "./Pages/Admin";
 
 class ClassDetails {
